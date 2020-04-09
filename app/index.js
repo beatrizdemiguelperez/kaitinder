07085a1f2cc9b2cdfb3e94aec878678c2b6f8549
Beatriz de miguel perez <beatriz.demiguel@kairosds.com>
2020-04-09 09:55:35 +0200

    challenge 1

--- app/index.js
+++ app/index.js
@@ -1,18 +1,50 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const swagger = require('./libs/swagger');
 const log = require('./libs/log');
 
 const port = process.env.PORT;
+const user = process.env.MONGO_USER;
+const pass = process.env.MONGO_PASS;
 
-const app = express();
+const mongoConfig = {
+  user,
+  pass,
+  socketTimeoutMS: 0,
+  keepAlive: true,
+  poolSize: 30,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello KaiTinder!');
+const initServer = () => {
+  const app = express();
+
+  app.get('/candidates', (req, res) => {
+    // @TODO: challenge-1 implementa la logica del endpoint
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    swagger.addSwaggerRoute(app, '/api-docs');
+  }
+
+  app.listen(port, (error) => (error ? log.error(error) : log.info(`Server listening at port: ${port}`)));
+};
+
+const { MONGO_HOST, MONGO_DB_NAME } = process.env;
+const mongoUri = `${MONGO_HOST}/${MONGO_DB_NAME}`;
+
+const mongoConnect = async () => {
+  try {
+    log.info(`Connecting to ${mongoUri}`);
+    // @TODO: challenge-1 implementa la conexión con mongo
+  } catch (e) {
+    log.error(`Error trying to connect Mongo database [${mongoUri}]: ${e}`, 'error');
+    throw e;
+  }
+};
+
+mongoConnect().then(() => {
+  initServer();
 });
-
-if (process.env.NODE_ENV === 'development') {
-  swagger.addSwaggerRoute(app, '/api-docs');
-}
-
-app.listen(port, (error) => (error ? log.error(error) : log.info(`Server listening at port: ${port}`)));
