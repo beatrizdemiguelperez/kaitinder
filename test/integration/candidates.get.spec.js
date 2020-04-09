8127c1f50bf0c8e2d5ba74af8ee87726dfae4f42
Beatriz de miguel perez <beatriz.demiguel@kairosds.com>
2020-04-09 13:50:26 +0200

    challenge 3 solved

--- test/integration/candidates.get.spec.js
+++ test/integration/candidates.get.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const nock = require('nock');
 
-const { setup, mocks } = require('./helpers');
+const { setup, mocks, teardown } = require('./helpers');
 
 const mockedCandidates = mocks.mocks;
 const path = '/candidates';
@@ -13,6 +13,10 @@ describe('GET', () => {
   beforeAll(async () => {
     env = await setup();
     ({ app } = env);
+  });
+
+  afterAll(async () => {
+    await teardown();
   });
 
   describe(path, () => {
@@ -27,9 +31,39 @@ describe('GET', () => {
       await mocks.deleteAll();
     });
 
-    it('exists', async () => {
-      // @TODO: challenge-2 Implementa los casos de test
-      expect(true).toEqual(true);
+    it('should throw a 400 BAD REQUEST error when query params are wrong', async (done) => {
+      const resp = await request(app)
+        .get(path)
+        .set('Accept', 'application/json')
+        .query({
+          offset: 'wrong',
+          limit: 'wrong',
+        })
+        .send();
+      expect(resp.status).toEqual(400);
+      expect(resp.body.data.errors.length).toEqual(2);
+      done();
+    });
+
+    describe.each([
+      [{ limit: 0, offset: 0 }, mockedCandidates],
+      [{ limit: 1, offset: 0 }, [mockedCandidates[0]]],
+      [{ limit: 1, offset: 1 }, [mockedCandidates[1]]],
+      [{ limit: 2, offset: 0 }, [mockedCandidates[0], mockedCandidates[1]]],
+    ])('%s', ({ limit, offset }, expected) => {
+      it(`should return candidates with limit ${limit} and offset ${offset}`, async (done) => {
+        const resp = await request(app)
+          .get(path)
+          .set('Accept', 'application/json')
+          .query({
+            offset,
+            limit,
+          })
+          .send();
+        expect(resp.status).toEqual(200);
+        expect(resp.body.data.map((i) => i.name)).toEqual(expected.map((i) => i.name));
+        done();
+      });
     });
   });
 });
