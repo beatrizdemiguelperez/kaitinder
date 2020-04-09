b4722a556c18ecdb3e6f75b7c4cf4d41e134d796
Victor Pose Murga <victorpmu@ext.zara.com>
2020-04-09 19:32:24 +0200

    unit tests

--- test/unit/domain/candidates.spec.js
+++ test/unit/domain/candidates.spec.js
@@ -1,9 +1,40 @@
-// const { createCandidate } = require('../../../../app/domain/candidates');
-
-// @TODO: test domain functions
+const { create } = require('../../../app/domain/candidates');
 
 describe('Unit tests for candidates domain', () => {
   describe('createCandidate function', () => {
-    it('should call functions and return persist result', async () => {});
+    it('should call functions and return persist result', async () => {
+      const insertFn = jest.fn().mockReturnValue({
+        city: 'Madrid',
+        name: 'Yo',
+        _id: '123123',
+      });
+
+      const getCitiesFn = jest.fn().mockReturnValue([
+        {
+          city: 'Madrid',
+          lat: 40.4,
+          long: -3.6834,
+        },
+      ]);
+      const candidate = {
+        city: 'Madrid',
+        name: 'Yo',
+      };
+      const result = await create(insertFn, getCitiesFn)(candidate);
+      expect(getCitiesFn).toBeCalledWith();
+      expect(insertFn).toBeCalledWith({
+        city: 'Madrid',
+        name: 'Yo',
+        coordinates: {
+          lat: 40.4,
+          long: -3.6834,
+        },
+      });
+      expect(result).toEqual({
+        city: 'Madrid',
+        name: 'Yo',
+        _id: '123123',
+      });
+    });
   });
 });
--- test/unit/domain/index.spec.js
+++ test/unit/domain/index.spec.js
@@ -1,7 +1,19 @@
-// @TODO: si da tiempo...
+const candidatesIndex = require('../../../app/domain');
+const candidatesDomain = require('../../../app/domain/candidates');
+const Candidate = require('../../../app/repository/mongo/candidates');
+const cities = require('../../../app/services/cities');
 
 describe('Unit tests for candidates domain', () => {
   describe('createCandidate route tests', () => {
-    it('should call candidates createCandidate with all parameters', async () => {});
+    it('should call candidates createCandidate with all parameters', async () => {
+      const secondFn = jest.fn().mockReturnValue('responseData');
+      candidatesDomain.create = jest.fn().mockReturnValue(secondFn);
+
+      const result = await candidatesIndex.create('newCandidate');
+
+      expect(candidatesDomain.create).toBeCalledWith(Candidate.insert, cities.getCities);
+      expect(secondFn).toBeCalledWith('newCandidate');
+      expect(result).toBe('responseData');
+    });
   });
 });
